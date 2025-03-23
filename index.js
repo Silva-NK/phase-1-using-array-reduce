26c0bfafa662d7c107f91496ed08a67f36c47fde
Silva-NK <kiagonsuzanne@gmail.com>
2025-03-23 16:19:06 +0300

    Creates variables totalBatteries and function to use the reduce method to sum batteryBatches

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = ourReduce(batteryBatches, (total, batch) => total + batch, 0);
+
+console.log(totalBatteries);
+
+function ourReduce(arr, reducer, init) {
+    let accum = init;
+    for (const element of arr) {
+        accum = reducer(accum, element);
+    }
+    return accum;
+}
